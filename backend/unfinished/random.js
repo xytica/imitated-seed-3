c9afce3ae7869fc4135751105b7623d6dd324392
xytica <161139475+xytica@users.noreply.github.com>
2024-05-15 19:26:31 +0900

    불필요한 파일 삭제
    https://github.com/gdl-blue/imitated-seed-2/issues/40#issuecomment-1637064160

--- backend/unfinished/random.js
+++
@@ -1,5 +0,0 @@
-router.get(/^\/random$/, async(req, res) => {
-	var data = await curs.execute("select title from documents where namespace = '문서' order by random() limit 1");
-	if(!data.length) res.redirect('/');
-	res.redirect('/w/' + encodeURIComponent(data[0].title));
-});
