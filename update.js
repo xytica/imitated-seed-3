e9e3d45b503800db8fbb1e766545e062455ef939
xytica <161139475+xytica@users.noreply.github.com>
2024-05-19 17:43:49 +0900

    파일 download 구현

--- update.js
+++ update.js
@@ -1,2 +1,5 @@
 import { createreadstream, createWriteStream } from 'node:fs';
 
+const updatedZipFileAddress = 'https://github.com/xytica/imitated-seed-3/archive/refs/heads/master.zip';
+
+const updatedFiles = await fetch(updatedZipFileAddress);
